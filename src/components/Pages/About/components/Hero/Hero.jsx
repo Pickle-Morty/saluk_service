bfee42c2f58756736cf40e6aa6f4af7671c7944b
Пользователь <ruslan.11.2000@mail.ru>
2022-09-01 18:07:42 +0600

    Add_Adaptiv.vol.2

--- src/components/Pages/About/components/Hero/Hero.jsx
+++ src/components/Pages/About/components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ import arrowLeft from "../../../../img/icons/arrowLeftYellow.png"
 const Title = () => {
 
     return (
-        <div>
+        <div className={styles.hero__container}>
             <div className={styles.about}>О нас</div>
             <div className={styles.title}>
                 Мы помогаем <span className={styles.color_green} >государству</span> и  <span className={styles.color_yellow}>бизнесу</span> воплощать свои идеи в жизнь
@@ -20,9 +20,9 @@ const Title = () => {
                 наша цель - создавать полезные и простые в использовании продукты и помогать людям развивать свой бизнес.
             </div>
             <div className={styles.box}>
-           <button className={styles.btn}>Давайте поговорим <img src={messageIcon} alt="" /></button>
-           <a href="" className={styles.linkBtn_m}>Посмотреть портфолио<img src={arrowLeft} alt="" /></a>
-        </div>
+                <button className={styles.btn}>Давайте поговорим <img src={messageIcon} alt="" /></button>
+                <a href="" className={styles.linkBtn_m}>Посмотреть портфолио<img src={arrowLeft} alt="" /></a>
+            </div>
         </div>
     )
 }
